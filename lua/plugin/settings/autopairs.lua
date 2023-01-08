9275ceef959d35125b514ea715ee30f4c8a9f4dd
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-08 22:26:48 +0900

    feat: enable autopairs when using cmp

--- lua/plugin/settings/autopairs.lua
+++ lua/plugin/settings/autopairs.lua
@@ -1,11 +1,17 @@
 return function()
     local module = require("utils.module")
     local is_ok, mods = pcall(module.require, {
+        "cmp",
         { "nvim-autopairs", as = "autopairs" },
+        { "nvim-autopairs.completion.cmp", as = "autopairs_cmp" }
     })
     if not is_ok then
         return
     end
 
     mods["autopairs"].setup()
+    mods["cmp"].event:on(
+        "confirm_done",
+        mods["autopairs_cmp"].on_confirm_done()
+    )
 end
