91307bcae8e3c2fc678bf00a6101632c6e7709a8
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-08 12:01:33 +0900

    feat: disable italic comment and enable better performance

--- lua/plugin/settings/sonokai.lua
+++ lua/plugin/settings/sonokai.lua
@@ -1,5 +1,10 @@
 return function()
+    -- Got a true color.
     vim.o.termguicolors = true
+
+    -- Settings for this colorscheme
     vim.g.sonokai_style = "shusia"
+    vim.g.sonokai_disable_italic_comment = true
+    vim.g.sonokai_better_performance = true
     vim.cmd.colorscheme("sonokai")
 end
