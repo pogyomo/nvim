fb6d421a2b47f5f3ea5b94d57a9c4f13100113d6
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-08 21:47:33 +0900

    feat: change settings of sonokai

--- lua/plugin/settings/sonokai.lua
+++ lua/plugin/settings/sonokai.lua
@@ -1,7 +1,17 @@
 return function()
-    -- Settings for this colorscheme
+    -- Change the style of this colorscheme.
     vim.g.sonokai_style = "shusia"
+
+    -- Disable italic.
+    vim.g.sonokai_enable_italic = false
     vim.g.sonokai_disable_italic_comment = true
+
+    -- Make virtual text to be colorful.
+    vim.g.sonokai_diagnostic_virtual_text = "colored"
+
+    -- Reduce the loading time.
     vim.g.sonokai_better_performance = true
+
+    -- Load this colorscheme
     vim.cmd.colorscheme("sonokai")
 end
