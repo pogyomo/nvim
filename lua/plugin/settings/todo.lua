bfaee2f1999e6e259a9fd99a64791f98ad852f87
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-09 19:31:53 +0900

    fix: add missing guard.

--- lua/plugin/settings/todo.lua
+++ lua/plugin/settings/todo.lua
@@ -3,6 +3,9 @@ return function()
     local is_ok, mods = pcall(module.require, {
         "todo-comments"
     })
+    if not is_ok then
+        return
+    end
 
     vim.o.signcolumn = "yes"
     mods["todo-comments"].setup{
