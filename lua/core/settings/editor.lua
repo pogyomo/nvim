0c1db1f5793db78491c4ad50729e9d8d485b6d53
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-10 12:17:30 +0900

    feat: add configs

--- lua/core/settings/editor.lua
+++ lua/core/settings/editor.lua
@@ -1,7 +1,22 @@
+-- Tab and indent
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.cindent = true
+
+-- Disable mouse
+vim.o.mouse = ""
+
+-- Config minimal spaces between cursor and top/bottom.
+vim.o.scrolloff = 5
+
+-- Visual
+vim.o.showcmd = false
 vim.o.number = true
 vim.o.cursorline = true
-vim.o.cindent = true
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
 vim.o.termguicolors = true
+
+-- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
