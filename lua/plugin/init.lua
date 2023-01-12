d4c9f96e3abbc0a9139df4ef297c5c26ee7b388b
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-12 19:19:20 +0900

    refactor: write the install process to plugin/init.lua directly
    I think modules in utils directory must be used in several places, but
    the install module is only used in plugin/init.lua.

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,2 +1,30 @@
-require("utils.install").install_lazy()
+local path      = require("utils.path")
+local uv        = vim.loop
+local lazy_url  = "https://github.com/folke/lazy.nvim"
+local lazy_path = path.build(vim.fn.stdpath("data"), {
+    "lazy",
+    "lazy.nvim"
+})
+
+-- Install lazy if not exist
+if not uv.fs_stat(lazy_path) then
+    vim.notify("Installing lazy...")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        lazy_url,
+        "--branch=stable",
+        lazy_path
+    })
+    assert(
+        vim.v.shell_error == 0,
+        ("Failed to install lazy: code is %d"):format(vim.v.shell_error)
+    )
+    vim.notify("Lazy was installed successfully.")
+    vim.opt.rtp:prepend(lazy_path)
+else
+    vim.opt.rtp:prepend(lazy_path)
+end
+
 require("lazy").setup("plugin.specs")
