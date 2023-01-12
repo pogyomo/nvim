57c88e5655f7e558804d0f2e133f2cf6ab0f3156
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-13 01:37:55 +0900

    feat: add submode.nvim

---
+++ lua/plugin/specs/submode.lua
@@ -0,0 +1,68 @@
+return {
+    "pogyomo/submode.nvim",
+    config = function()
+        local module = require("utils.module")
+        local mods = module.require{
+            "submode"
+        }
+
+        mods["submode"]:setup()
+
+        vim.g.mapleader = " "
+        vim.keymap.set("n", "<Leader>l", "<Plug>(submode-lsp-operator)")
+
+        mods["submode"]:create("LspOperator", {
+            mode  = "n",
+            enter = "<Plug>(submode-lsp-operator)",
+            leave = { "q", "<ESC>" }
+        }, {
+            lhs = "d",
+            rhs = vim.lsp.buf.definition
+        }, {
+            lhs = "D",
+            rhs = vim.lsp.buf.declaration
+        }, {
+            lhs = "H",
+            rhs = vim.lsp.buf.hover
+        }, {
+            lhs = "i",
+            rhs = vim.lsp.buf.implementation
+        }, {
+            lhs = "r",
+            rhs = vim.lsp.buf.references
+        })
+
+        mods["submode"]:create("DocReader", {
+            mode = "n"
+        }, {
+            lhs = "<Enter>",
+            rhs = "<C-]>"
+        }, {
+            lhs = "u",
+            rhs = "<cmd>po<cr>"
+        }, {
+            lhs = { "r", "U" },
+            rhs = "<cmd>ta<cr>"
+        }, {
+            lhs = "i",
+            rhs = "<cmd>lua require('submode'):leave()<cr><insert>"
+        })
+
+        vim.api.nvim_create_augroup("DocReaderAug", {})
+        vim.api.nvim_create_autocmd({
+            "BufEnter", "BufLeave"
+        }, {
+            group = "DocReaderAug",
+            pattern = "*",
+            callback = function(opt)
+                if vim.opt.ft:get() == "help" then
+                    if opt.event == "BufEnter" then
+                        mods["submode"]:enter("DocReader")
+                    else
+                        mods["submode"]:leave()
+                    end
+                end
+            end
+        })
+    end
+}
