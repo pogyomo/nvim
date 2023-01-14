2dd77257b02060ab5be5e515724b1e92fd57d6a1
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-14 17:58:29 +0900

    feat: add winresizer to submode

--- lua/plugin/specs/submode.lua
+++ lua/plugin/specs/submode.lua
@@ -10,9 +10,23 @@ return {
             return ("%s<cmd>lua require('submode'):leave()<cr>"):format(map)
         end
 
-        mods["submode"]:setup()
+        local function have_neighbor_to(window, dir)
+            local right = vim.api.nvim_win_call(window, function()
+                vim.cmd.wincmd(({
+                    left  = "h",
+                    right = "l",
+                    up    = "k",
+                    down  = "j"
+                })[dir])
+                return vim.api.nvim_get_current_win()
+            end)
+            return vim.api.nvim_win_get_number(right) == vim.api.nvim_win_get_number(window)
+        end
 
         vim.keymap.set("n", "<Leader>l", "<Plug>(submode-lsp-operator)", { remap = true })
+        vim.keymap.set("n", "<Leader>r", "<Plug>(submode-win-resizer)",  { remap = true })
+
+        mods["submode"]:setup()
 
         mods["submode"]:create("LspOperator", {
             mode  = "n",
@@ -33,6 +47,30 @@ return {
         }, {
             lhs = "r",
             rhs = function() vim.lsp.buf.references() end
+        })
+
+        mods["submode"]:create("WinResizer", {
+            mode = "n",
+            enter = "<Plug>(submode-win-resizer)",
+            leave = { "q", "<ESC>" }
+        }, {
+            lhs = { "l", "h", "j", "k" },
+            rhs = function(lhs)
+                local dir = (lhs == "l" or lhs == "h") and "width" or "height"
+                local setter = vim.api["nvim_win_set_" .. dir]
+                local getter = vim.api["nvim_win_get_" .. dir]
+                local diff = 5
+
+                if lhs == "l" then
+                    setter(0, getter(0) + (have_neighbor_to(0, "right") and -diff or diff))
+                elseif lhs == "h" then
+                    setter(0, getter(0) + (have_neighbor_to(0, "right") and diff or -diff))
+                elseif lhs == "j" then
+                    setter(0, getter(0) + (have_neighbor_to(0, "down")  and -diff or diff))
+                else
+                    setter(0, getter(0) + (have_neighbor_to(0, "down")  and diff or -diff))
+                end
+            end
         })
 
         mods["submode"]:create("DocReader", {
