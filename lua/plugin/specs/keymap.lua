2ab00c8a3c253a18841bbf093a4dd85e38604332
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-01-17 11:35:34 +0900

    refactor: change file structure

--- lua/plugin/specs/keymap.lua
+++ lua/plugin/specs/keymap.lua
@@ -1,6 +1,25 @@
 return {
     {
         "max397574/better-escape.nvim",
+        event = "InsertEnter",
         config = true
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            local module = require("utils.module")
+            local mods = module.require{
+                "cmp",
+                { "nvim-autopairs", as = "autopairs" },
+                { "nvim-autopairs.completion.cmp", as = "autopairs_cmp" }
+            }
+
+            mods["autopairs"].setup()
+            mods["cmp"].event:on(
+                "confirm_done",
+                mods["autopairs_cmp"].on_confirm_done()
+            )
+        end
     }
 }
