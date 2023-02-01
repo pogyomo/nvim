4ff6ee922800a25594a409310e7f3f500dd79dd3
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-02-01 12:39:54 +0900

    style: improve annotation comments

--- lua/utils/window/resize.lua
+++ lua/utils/window/resize.lua
@@ -1,5 +1,7 @@
----@param window integer
----@param dir "left" | "right" | "up" | "down"
+---Check whether the window have neighbor to 'dir' or not.
+---@param window integer Window handle, or 0 for current window.
+---@param dir "left" | "right" | "up" | "down" Direction to check.
+---@return boolean # True if the window have neighbor to 'dir'.
 local function have_neighbor_to(window, dir)
     local neighbor = vim.api.nvim_win_call(window, function()
         vim.cmd.wincmd(({
@@ -15,10 +17,11 @@ local function have_neighbor_to(window, dir)
     return n_winnr == w_winnr
 end
 
----@param win integer
----@param diff_row integer
----@param diff_col integer
----@param key_dir "left" | "right" | "up" | "down"
+---Resize the window as normal (not float) window.
+---@param win integer Window handle, or 0 for current window.
+---@param diff_row integer Diff to be used when resize width.
+---@param diff_col integer Diff to be used when resize height.
+---@param key_dir "left" | "right" | "up" | "down" Direction to resize.
 local function resize_normal(win, diff_row, diff_col, key_dir)
     local dir = (key_dir == "left" or key_dir == "right") and "width" or "height"
     local setter = vim.api["nvim_win_set_" .. dir]
@@ -39,10 +42,11 @@ local function resize_normal(win, diff_row, diff_col, key_dir)
     end
 end
 
----@param win integer
----@param diff_row integer
----@param diff_col integer
----@param key_dir "left" | "right" | "up" | "down"
+---Resize the window as floating window.
+---@param win integer Window handle, or 0 for current window.
+---@param diff_row integer Diff to be used when resize width.
+---@param diff_col integer Diff to be used when resize height.
+---@param key_dir "left" | "right" | "up" | "down" Direction to resize.
 local function resize_float(win, diff_row, diff_col, key_dir)
     local dir = (key_dir == "left" or key_dir == "right") and "width" or "height"
     local setter = vim.api["nvim_win_set_" .. dir]
@@ -55,10 +59,11 @@ local function resize_float(win, diff_row, diff_col, key_dir)
     end
 end
 
----@param win integer
----@param diff_row integer
----@param diff_col integer
----@param key_dir "left" | "right" | "up" | "down"
+---Resize the window.
+---@param win integer Window handle, or 0 for current window.
+---@param diff_row integer Diff to be used when resize width.
+---@param diff_col integer Diff to be used when resize height.
+---@param key_dir "left" | "right" | "up" | "down" Direction to resize.
 return function(win, diff_row, diff_col, key_dir)
     if vim.api.nvim_win_get_config(win).relative ~= "" then
         resize_float(win, diff_row, diff_col, key_dir)
