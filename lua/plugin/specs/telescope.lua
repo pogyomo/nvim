8bd122e8eba763b6c12c6b513b881346937c6387
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-02-02 21:20:35 +0900

    fix: add nvim-devicons as dependencies of telescope

--- lua/plugin/specs/telescope.lua
+++ lua/plugin/specs/telescope.lua
@@ -2,7 +2,8 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.1",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons"
     },
     cmd = "Telescope",
     opts = {
