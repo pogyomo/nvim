4f1264d89bf85092740007d2618b2aa006b15581
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-02-02 18:41:51 +0900

    feat: add telescope

---
+++ lua/plugin/specs/telescope.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+    cmd = "Telescope",
+    opts = {
+        defaults = {
+            winblend = 10
+        }
+    }
+}
