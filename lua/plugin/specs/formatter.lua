73b61ccfbb5ffa1f71b7dd681e22f56486bdd682
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-11-23 01:53:55 +0900

    feat: automaticall install formatter

--- lua/plugin/specs/formatter.lua
+++ lua/plugin/specs/formatter.lua
@@ -2,6 +2,9 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
+    dependencies = {
+        "williamboman/mason.nvim",
+    },
     opts = {
         formatters_by_ft = {
             rust = { "rustfmt" },
@@ -13,4 +16,34 @@ return {
             timeout_ms = 500,
         },
     },
+    config = function(_, opts)
+        local module = require("utils.module")
+        local mods = module.require {
+            "conform",
+            "mason-registry",
+        }
+
+        local ensure_installed = {
+            "clang-format",
+            "stylua",
+        }
+
+        for _, name in ipairs(ensure_installed) do
+            local pkg = mods["mason-registry"].get_package(name)
+            if pkg:is_installed() then
+                goto continue
+            end
+            vim.notify(("[formatter.lua] installing %s"):format(name))
+            pkg:install():once("closed", function()
+                if pkg:is_installed() then
+                    vim.notify(("[formatter.lua] %s was successfully installed"):format(name))
+                else
+                    vim.notify(("[formatter.lua] failed to install %s"):format(name), vim.log.levels.ERROR)
+                end
+            end)
+            ::continue::
+        end
+
+        mods["conform"].setup(opts)
+    end,
 }
