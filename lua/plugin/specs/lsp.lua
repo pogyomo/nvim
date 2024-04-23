13111c24e5e213818ab2576a9f2906370544ef80
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-04-24 04:52:46 +0900

    feat: add gdscript lsp

--- lua/plugin/specs/lsp.lua
+++ lua/plugin/specs/lsp.lua
@@ -30,6 +30,13 @@ return {
         -- Default config of each lsp.
         local capabilities = mods["cmp_nvim_lsp"].default_capabilities()
 
+        -- mason-lspconfig doesn't manage gdscript, so manually configure the lsp.
+        mods["lspconfig"].gdscript.setup {
+            capabilities = capabilities,
+            -- FIXME: If we write this using vim.lsp.rpc.connect instead of using `ncat`, this doesn't works well.
+            cmd = { "ncat", "127.0.0.1", "6005" },
+        }
+
         mods["mason-lspconfig"].setup_handlers {
             function(name)
                 mods["lspconfig"][name].setup {
