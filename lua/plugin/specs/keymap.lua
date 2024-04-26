5793935fea6d023602d3c20382f5071cf2a3c83c
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-04-26 12:44:28 +0900

    feat!: remove better-escape.nvim

--- lua/plugin/specs/keymap.lua
+++ lua/plugin/specs/keymap.lua
@@ -140,13 +140,4 @@ return {
             })
         end,
     },
-    {
-        "max397574/better-escape.nvim",
-        opts = {
-            mapping = { "jj" },
-            timeout = vim.o.timeoutlen,
-            clear_empty_lines = false,
-            keys = "<Esc>",
-        },
-    },
 }
