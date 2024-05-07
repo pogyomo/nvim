340a7abb13516c4f790528515a5b888c12e16407
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-05-07 18:04:09 +0900

    feat: add neogit for git integration

---
+++ lua/plugin/specs/git.lua
@@ -0,0 +1,10 @@
+return {
+    "NeogitOrg/neogit",
+    branch = "nightly",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = {},
+}
