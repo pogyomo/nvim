41e626a787a1001dd9facf9ea2171b8c17ea83f3
pogyomo <pogyomo@gmail.com>
2024-05-16 01:44:59 +0900

    feat: change appearance of lualine

--- lua/plugin/specs/lualine.lua
+++ lua/plugin/specs/lualine.lua
@@ -10,40 +10,6 @@ return {
             "submode",
         }
 
-        local function is_in_range(l, r, value)
-            return l <= value and value < r
-        end
-
-        local function current_time_clock_emoji()
-            local time = os.date("*t")
-            local hour = time.hour
-            if is_in_range(0, 1, hour) or is_in_range(12, 13, hour) then
-                return "󱑖 "
-            elseif is_in_range(1, 2, hour) or is_in_range(13, 14, hour) then
-                return "󱑋 "
-            elseif is_in_range(2, 3, hour) or is_in_range(14, 15, hour) then
-                return "󱑌 "
-            elseif is_in_range(3, 4, hour) or is_in_range(15, 16, hour) then
-                return "󱑍 "
-            elseif is_in_range(4, 5, hour) or is_in_range(16, 17, hour) then
-                return "󱑎 "
-            elseif is_in_range(5, 6, hour) or is_in_range(17, 18, hour) then
-                return "󱑏 "
-            elseif is_in_range(6, 7, hour) or is_in_range(18, 19, hour) then
-                return "󱑐 "
-            elseif is_in_range(7, 8, hour) or is_in_range(19, 20, hour) then
-                return "󱑑 "
-            elseif is_in_range(8, 9, hour) or is_in_range(20, 21, hour) then
-                return "󱑒 "
-            elseif is_in_range(9, 10, hour) or is_in_range(21, 22, hour) then
-                return "󱑓 "
-            elseif is_in_range(10, 11, hour) or is_in_range(22, 23, hour) then
-                return "󱑔 "
-            elseif is_in_range(11, 12, hour) or is_in_range(23, 24, hour) then
-                return "󱑕 "
-            end
-        end
-
         local filename_symbols = {
             modified = "+",
             readonly = "-",
@@ -86,17 +52,17 @@ return {
         local status_tab = {
             lualine_a = {},
             lualine_b = {
-                { "tabs", max_length = vim.o.columns, mode = 2 },
+                { "buffers", max_length = vim.o.columns, mode = 2 },
             },
             lualine_c = {},
             lualine_x = {},
             lualine_y = {
+                { "tabs", max_length = vim.o.columns, mode = 0 },
                 {
                     function()
-                        local clock = current_time_clock_emoji()
-                        local date = vim.fn.strftime("%Y/%m/%d %H:%M:%S")
-                        return ("%s %s"):format(clock, date)
+                        return vim.fn.strftime("%Y/%m/%d %H:%M:%S")
                     end,
+                    separator = { left = "" },
                     color = "lualine_b_normal",
                 },
             },
