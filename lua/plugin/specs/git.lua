38e83b6ba29b3030ece95d726d79227e3bb886a0
pogyomo <pogyomo@gmail.com>
2024-05-22 00:31:17 +0900

    fix(git): set option for neogit

--- lua/plugin/specs/git.lua
+++ lua/plugin/specs/git.lua
@@ -7,4 +7,15 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     opts = {},
+    config = function(_, opts)
+        local module = require("utils.module")
+        local mods = module.require {
+            "neogit",
+        }
+
+        -- Temporary solution for https://github.com/NeogitOrg/neogit/issues/1285
+        vim.o.splitbelow = true
+
+        mods["neogit"].setup(opts)
+    end,
 }
