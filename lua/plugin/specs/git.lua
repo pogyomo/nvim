db060c5e9490d36712c6314ad202fc7be5d10d45
pogyomo <pogyomo@gmail.com>
2024-05-23 18:17:00 +0900

    fix: only enable splitbelow when in Neogit*

--- lua/plugin/specs/git.lua
+++ lua/plugin/specs/git.lua
@@ -14,7 +14,21 @@ return {
         }
 
         -- Temporary solution for https://github.com/NeogitOrg/neogit/issues/1285
-        vim.o.splitbelow = true
+        local group = vim.api.nvim_create_augroup("neogit-temp-solution", {})
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+            group = group,
+            pattern = "Neogit*",
+            callback = function()
+                vim.o.splitbelow = true
+            end,
+        })
+        vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
+            group = group,
+            pattern = "Neogit*",
+            callback = function()
+                vim.o.splitbelow = false
+            end,
+        })
 
         mods["neogit"].setup(opts)
     end,
