1d39d888740016380d3b67bc51798e48c7cfead8
pogyomo <pogyomo@gmail.com>
2024-05-28 14:36:24 +0900

    refactor: merge colorscheme.lua to visual.lua

--- lua/plugin/specs/visual.lua
+++ lua/plugin/specs/visual.lua
@@ -92,4 +92,17 @@ return {
         "j-hui/fidget.nvim",
         opts = {},
     },
+    {
+        "folke/tokyonight.nvim",
+        opts = { style = "storm" },
+        config = function(_, opts)
+            local module = require("utils.module")
+            local mods = module.require {
+                "tokyonight",
+            }
+
+            mods["tokyonight"].setup(opts)
+            vim.cmd.colorscheme("tokyonight")
+        end,
+    },
 }
