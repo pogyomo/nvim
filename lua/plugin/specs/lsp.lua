67088bab04b7043f4b73f84ca87e3458d3e686dd
pogyomo <pogyomo@gmail.com>
2024-05-31 20:47:01 +0900

    refactor: one line

--- lua/plugin/specs/lsp.lua
+++ lua/plugin/specs/lsp.lua
@@ -25,9 +25,7 @@ return {
 
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "gf", function()
-                    mods["conform"].format {
-                        async = true,
-                    }
+                    mods["conform"].format { async = true }
                 end, opts)
                 vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
