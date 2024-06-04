188a2b0da5d76e711c6344aefde4e916e5ec5a0b
pogyomo <pogyomo@gmail.com>
2024-06-05 00:50:14 +0900

    feat: neodev.nvim -> lazydev.nvim

--- lua/plugin/specs/lsp.lua
+++ lua/plugin/specs/lsp.lua
@@ -1,107 +1,116 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "folke/neodev.nvim",
-        "stevearc/conform.nvim",
-    },
-    config = function()
-        local module = require("utils.module")
-        local mods = module.require {
-            "lspconfig",
-            "mason-lspconfig",
-            "cmp_nvim_lsp",
-            "neodev",
-            "conform",
-        }
-
-        -- Keymaps for lsp actions
-        -- reference: https://zenn.dev/botamotch/articles/21073d78bc68bf
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("register-lsp-keymaps", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf }
-
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gf", function()
-                    mods["conform"].format { async = true }
-                end, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-                vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
-                vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
-                vim.keymap.set("n", "ge", vim.diagnostic.open_float, opts)
-                vim.keymap.set("n", "g]", function()
-                    vim.diagnostic.jump { count = 1 }
-                end, opts)
-                vim.keymap.set("n", "g[", function()
-                    vim.diagnostic.jump { count = -1 }
-                end, opts)
-            end,
-        })
-
-        -- Setup neodev before lspconfig
-        mods["neodev"].setup()
-
-        -- Specify lsps to install
-        mods["mason-lspconfig"].setup {
-            ensure_installed = {
-                "clangd",
-                "lua_ls",
-                "rust_analyzer",
-                "denols",
-                "pylsp",
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        dependencies = {
+            "Bilal2453/luvit-meta",
+        },
+        opts = {
+            library = {
+                "luvit-meta/library",
             },
-        }
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            "stevearc/conform.nvim",
+        },
+        config = function()
+            local module = require("utils.module")
+            local mods = module.require {
+                "lspconfig",
+                "mason-lspconfig",
+                "cmp_nvim_lsp",
+                "conform",
+            }
 
-        -- Default config of each lsp.
-        local capabilities = mods["cmp_nvim_lsp"].default_capabilities()
+            -- Keymaps for lsp actions
+            -- reference: https://zenn.dev/botamotch/articles/21073d78bc68bf
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("register-lsp-keymaps", {}),
+                callback = function(ev)
+                    local opts = { buffer = ev.buf }
 
-        -- mason-lspconfig doesn't manage gdscript, so manually configure the lsp.
-        mods["lspconfig"].gdscript.setup {
-            capabilities = capabilities,
-        }
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gf", function()
+                        mods["conform"].format { async = true }
+                    end, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "ge", vim.diagnostic.open_float, opts)
+                    vim.keymap.set("n", "g]", function()
+                        vim.diagnostic.jump { count = 1 }
+                    end, opts)
+                    vim.keymap.set("n", "g[", function()
+                        vim.diagnostic.jump { count = -1 }
+                    end, opts)
+                end,
+            })
 
-        mods["mason-lspconfig"].setup_handlers {
-            function(name)
-                mods["lspconfig"][name].setup {
-                    capabilities = capabilities,
-                }
-            end,
-            ["lua_ls"] = function()
-                mods["lspconfig"].lua_ls.setup {
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            workspace = {
-                                checkThirdParty = false,
-                            },
-                            runtime = {
-                                version = "LuaJIT",
-                            },
-                            diagnostics = {
-                                globals = { "vim" },
+            -- Specify lsps to install
+            mods["mason-lspconfig"].setup {
+                ensure_installed = {
+                    "clangd",
+                    "lua_ls",
+                    "rust_analyzer",
+                    "denols",
+                    "pylsp",
+                },
+            }
+
+            -- Default config of each lsp.
+            local capabilities = mods["cmp_nvim_lsp"].default_capabilities()
+
+            -- mason-lspconfig doesn't manage gdscript, so manually configure the lsp.
+            mods["lspconfig"].gdscript.setup {
+                capabilities = capabilities,
+            }
+
+            mods["mason-lspconfig"].setup_handlers {
+                function(name)
+                    mods["lspconfig"][name].setup {
+                        capabilities = capabilities,
+                    }
+                end,
+                ["lua_ls"] = function()
+                    mods["lspconfig"].lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                workspace = {
+                                    checkThirdParty = false,
+                                },
+                                runtime = {
+                                    version = "LuaJIT",
+                                },
+                                diagnostics = {
+                                    globals = { "vim" },
+                                },
                             },
                         },
-                    },
-                }
-            end,
-            ["rust_analyzer"] = function()
-                mods["lspconfig"].rust_analyzer.setup {
-                    capabilities = capabilities,
-                    settings = {
-                        ["rust-analyzer"] = {
-                            check = {
-                                command = "clippy",
+                    }
+                end,
+                ["rust_analyzer"] = function()
+                    mods["lspconfig"].rust_analyzer.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            ["rust-analyzer"] = {
+                                check = {
+                                    command = "clippy",
+                                },
                             },
                         },
-                    },
-                }
-            end,
-        }
-    end,
+                    }
+                end,
+            }
+        end,
+    },
 }
