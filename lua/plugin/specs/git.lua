285a1f9ef74774a001d7c3efa8fff2446ba2d036
pogyomo <pogyomo@gmail.com>
2024-06-11 00:26:00 +0900

    feat: remove autocmds for Neogit

--- lua/plugin/specs/git.lua
+++ lua/plugin/specs/git.lua
@@ -17,23 +17,6 @@ return {
             "neogit",
         }
 
-        -- Temporary solution for https://github.com/NeogitOrg/neogit/issues/1285
-        local group = vim.api.nvim_create_augroup("neogit-temp-solution", {})
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-            group = group,
-            pattern = "Neogit*",
-            callback = function()
-                vim.o.splitbelow = true
-            end,
-        })
-        vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
-            group = group,
-            pattern = "Neogit*",
-            callback = function()
-                vim.o.splitbelow = false
-            end,
-        })
-
         mods["neogit"].setup(opts)
     end,
 }
