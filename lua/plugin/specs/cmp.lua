1a368e34ec6b86cd7d66302f1826c1951ca749e7
pogyomo <pogyomo@gmail.com>
2024-07-23 06:12:04 +0900

    refactor: separate plugin

--- lua/plugin/specs/cmp.lua
+++ lua/plugin/specs/cmp.lua
@@ -1,113 +1,119 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-        -- Snippet engine
-        "L3MON4D3/LuaSnip",
-
-        -- Snippent
-        { "pogyomo/cppguard.nvim", dev = true },
-
-        -- Sources
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-        "saadparwaiz1/cmp_luasnip",
-
-        -- Visual
-        "onsails/lspkind.nvim",
+    {
+        "pogyomo/cppguard.nvim",
+        dev = true,
     },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
+    {
+        "hrsh7th/nvim-cmp",
+        event = { "InsertEnter", "CmdlineEnter" },
+        dependencies = {
+            -- Snippet engine
+            "L3MON4D3/LuaSnip",
 
-        luasnip.add_snippets("cpp", {
-            require("cppguard").snippet_luasnip("guard"),
-        })
+            -- Snippent
+            "pogyomo/cppguard.nvim",
 
-        -- Settings for insert mode.
-        cmp.setup {
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            window = {
-                completion = {
-                    winblend = 10,
+            -- Sources
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "saadparwaiz1/cmp_luasnip",
+
+            -- Visual
+            "onsails/lspkind.nvim",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            luasnip.add_snippets("cpp", {
+                require("cppguard").snippet_luasnip("guard"),
+            })
+
+            -- Settings for insert mode.
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
                 },
-                documentation = {
-                    winblend = 10,
-                    border = "rounded",
-                },
-            },
-            ---@diagnostic disable-next-line
-            formatting = {
-                format = require("lspkind").cmp_format {
-                    mode = "symbol_text",
-                    -- List all possible source name
-                    menu = {
-                        buffer = "[Buffer]",
-                        cmdline = "[Cmd]",
-                        lazydev = "[LazyDev]",
-                        luasnip = "[LuaSnip]",
-                        neorg = "[Neorg]",
-                        nvim_lsp = "[LSP]",
-                        nvim_lsp_signature_help = "[Signature]",
-                        path = "[Path]",
+                window = {
+                    completion = {
+                        winblend = 10,
+                    },
+                    documentation = {
+                        winblend = 10,
+                        border = "rounded",
                     },
                 },
-            },
-            mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<CR>"] = cmp.mapping.confirm { select = false },
-                ["<C-n>"] = cmp.mapping.select_next_item {
-                    behavior = cmp.SelectBehavior.Select,
+                ---@diagnostic disable-next-line
+                formatting = {
+                    format = require("lspkind").cmp_format {
+                        mode = "symbol_text",
+                        -- List all possible source name
+                        menu = {
+                            buffer = "[Buffer]",
+                            cmdline = "[Cmd]",
+                            lazydev = "[LazyDev]",
+                            luasnip = "[LuaSnip]",
+                            neorg = "[Neorg]",
+                            nvim_lsp = "[LSP]",
+                            nvim_lsp_signature_help = "[Signature]",
+                            path = "[Path]",
+                        },
+                    },
                 },
-                ["<C-p>"] = cmp.mapping.select_prev_item {
-                    behavior = cmp.SelectBehavior.Select,
+                mapping = {
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<CR>"] = cmp.mapping.confirm { select = false },
+                    ["<C-n>"] = cmp.mapping.select_next_item {
+                        behavior = cmp.SelectBehavior.Select,
+                    },
+                    ["<C-p>"] = cmp.mapping.select_prev_item {
+                        behavior = cmp.SelectBehavior.Select,
+                    },
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 },
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "nvim_lsp_signature_help" },
-                { name = "lazydev" },
-                { name = "neorg" },
-            }, {
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        }
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "lazydev" },
+                    { name = "neorg" },
+                }, {
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            }
 
-        -- Command line settings (when "/" or "?")
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
+            -- Command line settings (when "/" or "?")
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
 
-        -- Command line settings (when ":")
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-            }, {
-                { name = "cmdline" },
-            }),
-        })
-    end,
+            -- Command line settings (when ":")
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
+        end,
+    },
 }
