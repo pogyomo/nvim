527bf1e649e1b3180f4d9afeafcad6502123bbe6
pogyomo <pogyomo@gmail.com>
2025-04-20 02:01:26 +0900

    feat: don't provide keymaps for help when editing it

--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -1,3 +1,8 @@
+-- Provide below keymaps only when reading help.
+if vim.o.modifiable then
+    return
+end
+
 vim.keymap.set("n", "<Enter>", "<C-]>", { buffer = 0 })
 vim.keymap.set("n", "u", "<cmd>po<cr>", { buffer = 0 })
 vim.keymap.set("n", "r", "<cmd>ta<cr>", { buffer = 0 })
