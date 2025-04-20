8f9a823bea74b1c5ba69d14b01ad5df4a6429994
pogyomo <pogyomo@gmail.com>
2025-04-20 14:38:39 +0900

    refactor: move code for quickfix to init in nvim-bqf

--- lua/config/editor.lua
+++ lua/config/editor.lua
@@ -49,9 +49,6 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 -- Folding
 vim.o.foldmethod = "marker"
 
--- Quickfix
-vim.o.qftf = "{info -> v:lua._G.qftf(info)}"
-
 -- Diagnostic
 vim.diagnostic.config {
     virtual_text = true,
@@ -65,73 +62,3 @@ vim.diagnostic.config {
     },
     severity_sort = true,
 }
-
----Formatting function can be used in `qftf`.
----@param info table
----@return string[]
-function _G.qftf(info)
-    local items
-    if info.quickfix == 1 then
-        items = vim.fn.getqflist({ id = info.id, items = 0 }).items
-    else
-        items = vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
-    end
-
-    ---Append spaces to the left of `s` so its length become `n`.
-    ---@param s string
-    ---@param n integer
-    ---@return string
-    local function lalign(s, n)
-        while s:len() < n do
-            s = " " .. s
-        end
-        return s
-    end
-
-    ---Append spaces to the right of `s` so its length become `n`.
-    ---@param s string
-    ---@param n integer
-    ---@return string
-    local function ralign(s, n)
-        while s:len() < n do
-            s = s .. " "
-        end
-        return s
-    end
-
-    local elems = {}
-    local fname_width = 0
-    local lnum_width = 0
-    local col_width = 0
-    for idx = info.start_idx, info.end_idx, 1 do
-        local item = items[idx]
-
-        local fname = vim.fn.bufname(item.bufnr):gsub("^" .. vim.env.HOME, "~")
-        fname_width = math.max(fname_width, fname:len())
-
-        local lnum = string.format("%d", item.lnum)
-        lnum_width = math.max(lnum_width, lnum:len())
-
-        local col = string.format("%d", item.col)
-        col_width = math.max(col_width, col:len())
-
-        elems[#elems + 1] = {
-            fname = fname,
-            lnum = lnum,
-            col = col,
-            text = item.text,
-        }
-    end
-
-    local ret = {}
-    for _, elem in ipairs(elems) do
-        local fname = ralign(elem.fname, fname_width)
-        local lnum = lalign(elem.lnum, lnum_width)
-        local col = ralign(elem.col, col_width)
-
-        ret[#ret + 1] =
-            string.format("%s │%s:%s│ %s", fname, lnum, col, elem.text)
-    end
-
-    return ret
-end
--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -78,6 +78,79 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         opts = {},
+        init = function()
+            -- Better visual for quickfix
+            vim.o.qftf = "{info -> v:lua._G.qftf(info)}"
+
+            ---Formatting function can be used in `qftf`.
+            ---@param info table
+            ---@return string[]
+            function _G.qftf(info)
+                local items
+                if info.quickfix == 1 then
+                    items = vim.fn.getqflist({ id = info.id, items = 0 }).items
+                else
+                    items = vim.fn.getloclist(
+                        info.winid,
+                        { id = info.id, items = 0 }
+                    ).items
+                end
+
+                local function spaces(s, n, left)
+                    while s:len() < n do
+                        if left then
+                            s = " " .. s
+                        else
+                            s = s .. " "
+                        end
+                    end
+                    return s
+                end
+
+                local elems = {}
+                local fname_width = 0
+                local lnum_width = 0
+                local col_width = 0
+                for idx = info.start_idx, info.end_idx, 1 do
+                    local item = items[idx]
+
+                    local fname = vim.fn
+                        .bufname(item.bufnr)
+                        :gsub("^" .. vim.env.HOME, "~")
+                    fname_width = math.max(fname_width, fname:len())
+
+                    local lnum = string.format("%d", item.lnum)
+                    lnum_width = math.max(lnum_width, lnum:len())
+
+                    local col = string.format("%d", item.col)
+                    col_width = math.max(col_width, col:len())
+
+                    elems[#elems + 1] = {
+                        fname = fname,
+                        lnum = lnum,
+                        col = col,
+                        text = item.text,
+                    }
+                end
+
+                local ret = {}
+                for _, elem in ipairs(elems) do
+                    local fname = spaces(elem.fname, fname_width, false)
+                    local lnum = spaces(elem.lnum, lnum_width, true)
+                    local col = spaces(elem.col, col_width, false)
+
+                    ret[#ret + 1] = string.format(
+                        "%s │%s:%s│ %s",
+                        fname,
+                        lnum,
+                        col,
+                        elem.text
+                    )
+                end
+
+                return ret
+            end
+        end,
     },
     {
         "chomosuke/typst-preview.nvim",
