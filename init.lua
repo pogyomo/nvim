13a206bea062aeaada18048746a0a43303df66eb
pogyomo <pogyomo@gmail.com>
2025-06-15 18:32:16 +0900

    refactor: remove unused settings

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("config.editor")
 require("config.keymap")
-require("config.neovide")
 require("config.lazy")
--- lua/config/neovide.lua
+++
@@ -1,8 +0,0 @@
-if not vim.g.neovide then
-    return
-end
-
-vim.o.guifont = "HackGen Console NF:h12"
-vim.g.neovide_scroll_animation_length = 0
-vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_position_animation_length = 0
