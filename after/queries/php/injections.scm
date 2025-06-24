815906407c76cae106353f1907e921f85673addd
pogyomo <pogyomo@gmail.com>
2025-06-24 20:52:52 +0900

    fix: parse single quoted string as sql

--- after/queries/php/injections.scm
+++ after/queries/php/injections.scm
@@ -1,31 +1,25 @@
 ;; extends
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*SELECT")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*SELECT")
+                  (#set! injection.language "sql"))
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*INSERT")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*INSERT")
+                  (#set! injection.language "sql"))
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*UPDATE")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*UPDATE")
+                  (#set! injection.language "sql"))
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*DELETE")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*DELETE")
+                  (#set! injection.language "sql"))
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*CREATE")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*CREATE")
+                  (#set! injection.language "sql"))
 
-(encapsed_string
-  (string_content) @injection.content
-  (#lua-match? @injection.content "^%s*ALTER")
-  (#set! injection.language "sql"))
+((string_content) @injection.content
+                  (#lua-match? @injection.content "^%s*ALTER")
+                  (#set! injection.language "sql"))
