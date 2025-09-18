7600b2e9fd38b521e00d9d284815006b4c7d7c97
pogyomo <pogyomo@gmail.com>
2025-09-18 23:08:11 +0900

    feat: add dial.nvim

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -63,4 +63,65 @@ return {
             })
         end,
     },
+    {
+        "monaqa/dial.nvim",
+        keys = {
+            {
+                "<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "normal")
+                end,
+                mode = "n",
+            },
+            {
+                "<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "normal")
+                end,
+                mode = "n",
+            },
+            {
+                "g<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "gnormal")
+                end,
+                mode = "n",
+            },
+            {
+                "g<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "gnormal")
+                end,
+                mode = "n",
+            },
+            {
+                "<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "visual")
+                end,
+                mode = "x",
+            },
+            {
+                "<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "visual")
+                end,
+                mode = "x",
+            },
+            {
+                "g<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "gvisual")
+                end,
+                mode = "x",
+            },
+            {
+                "g<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "gvisual")
+                end,
+                mode = "x",
+            },
+        },
+    },
 }
