d91aa27ca24d9ee3721499ef8a5daf855851af44
pogyomo <pogyomo@gmail.com>
2026-04-01 00:05:07 +0900

    docs: add comments

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -17,20 +17,23 @@ return {
             end
         end
 
-        -- Run linter on write.
+        -- Run linter on write
         vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
             group = vim.api.nvim_create_augroup("pogyomo.linter", {}),
             callback = function()
+                -- Check if linter exists for the file
                 local bufnr = vim.api.nvim_get_current_buf()
                 local ft = vim.bo[bufnr].filetype
                 if linters_by_ft[ft] == nil then
                     return
                 end
 
+                -- Execute linters in series
                 local linters = linters_by_ft[vim.bo[bufnr].filetype]
                 for _, linter in ipairs(linters) do
                     local opts = {}
                     if linter == "selene" then
+                        -- selene needs to be executed where selene.toml exists
                         opts["cwd"] = vim.fs.root(bufnr, "selene.toml")
                     end
                     lint.try_lint(linter, opts)
