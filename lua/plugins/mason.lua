cca11db8d444e4a3d0187b265a9b0ea0c9487df4
pogyomo <pogyomo@gmail.com>
2026-07-09 21:47:09 +0900

    docs: todo

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,8 +8,8 @@ return {
     },
     config = function(_, opts)
         -- TODO:
-        -- roslyn-language-server is available, but install via mason will crash.
-        -- It seems installed path is too long to handle by windows, so need to change install dir for windows?
+        -- roslyn-language-server is available, but the one installed via mason will crash on Windows.
+        -- It seems installed path is too long to be handled by Windows. Need to change install destination?
         require("mason").setup(opts)
 
         local event = require("helpers.event")
