6e6e1380c34dcc2600d815f2d19a72259b71e4a6
pogyomo <pogyomo@gmail.com>
2026-07-10 22:22:02 +0900

    refactor: move create_autocmd outside of event.once

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,42 +31,32 @@ return {
             local global_settings = settings.get_global_settings()
             local ft_settings = settings.get_ft_settings()
 
+            -- Keymaps for lsp actions
+            -- reference: https://zenn.dev/botamotch/articles/21073d78bc68bf
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("pogyomo.lsp", {}),
+                callback = function(ev)
+                    local opts = { buffer = ev.buf }
+
+                    vim.keymap.set("n", "K", function()
+                        vim.lsp.buf.hover { border = "rounded" }
+                    end, opts)
+                    vim.keymap.set("n", "gf", function()
+                        require("conform").format { async = true }
+                    end, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
+                end,
+            })
+
             -- Load lsp config after mason installed servers.
             -- Lsp will starts after servers installed without opening file.
             event.once("auto_install_finished", function()
-                -- Keymaps for lsp actions
-                -- reference: https://zenn.dev/botamotch/articles/21073d78bc68bf
-                vim.api.nvim_create_autocmd("LspAttach", {
-                    group = vim.api.nvim_create_augroup("pogyomo.lsp", {}),
-                    callback = function(ev)
-                        local opts = { buffer = ev.buf }
-
-                        vim.keymap.set("n", "K", function()
-                            vim.lsp.buf.hover { border = "rounded" }
-                        end, opts)
-                        vim.keymap.set("n", "gf", function()
-                            require("conform").format { async = true }
-                        end, opts)
-                        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                        vim.keymap.set(
-                            "n",
-                            "gi",
-                            vim.lsp.buf.implementation,
-                            opts
-                        )
-                        vim.keymap.set(
-                            "n",
-                            "gt",
-                            vim.lsp.buf.type_definition,
-                            opts
-                        )
-                        vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
-                        vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
-                    end,
-                })
-
                 -- Add capabilities for all server.
                 vim.lsp.config("*", {
                     capabilities = require("cmp_nvim_lsp").default_capabilities(),
